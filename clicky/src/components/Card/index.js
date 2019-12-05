9e8a5aaf9ddfcc1454cba5f8b84f070acd3ef12f
jastring <tripp.stringfield@gmail.com>
2019-12-05 17:40:09 -0500

    setup react components

---
+++ clicky/src/components/Card/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+function Card(props){
+    return (
+
+
+    )
+}
+
+export default Card;
---
+++ clicky/src/components/Scoreboard/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style.css";
+
+function Scoreboard(props){
+    return(
+
+
+    )
+}
+
+export default Scoreboard;
