bdd76bd450fb9f88c2d40fd181d930bd5dcdccc9
jastring <tripp.stringfield@gmail.com>
2019-12-07 18:44:23 -0500

    Updates

---
+++ precache-manifest.2ddc064a1f887f6692a6f30d907642d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76597beb71ed98a0477bb0b77c7c4d30",
+    "url": "/Clicky/index.html"
+  },
+  {
+    "revision": "48c85e365c9f762b6f0b",
+    "url": "/Clicky/static/css/main.833a3a9a.chunk.css"
+  },
+  {
+    "revision": "3562be755aa2ec3737d8",
+    "url": "/Clicky/static/js/2.b1f5d4ef.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Clicky/static/js/2.b1f5d4ef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "48c85e365c9f762b6f0b",
+    "url": "/Clicky/static/js/main.8da9c9c0.chunk.js"
+  },
+  {
+    "revision": "0a7e6715a2a03890c448",
+    "url": "/Clicky/static/js/runtime-main.6b4e36c6.js"
+  }
+]);
